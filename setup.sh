18866ac5d0c4654d5f4ad95289f155427c476aa0
Kevin Haffner <khaffner@gmail.com>
2019-06-04 20:26:02 +0200

    make symlink for pwsh

--- setup.sh
+++ setup.sh
@@ -9,6 +9,7 @@ sudo apt install libunwind8 -y
 wget https://github.com/PowerShell/PowerShell/releases/download/v6.2.1/powershell-6.2.1-linux-arm32.tar.gz
 mkdir ~/powershell
 tar -xvf ./powershell-6.2.1-linux-arm32.tar.gz -C ~/powershell
+sudo ln -s ~/powershell/pwsh /usr/bin/pwsh
 
 # Set baud rade, assuming device is ttyUSB0
 sudo stty -F /dev/ttyUSB0 4800 # Not permanent?

15121dee41b6ea414a5df3b58bd88aaa3d7753b5
Kevin Haffner <khaffner@gmail.com>
2019-06-05 07:46:42 +0200

    own gps variables and fixed link

--- Get-BoatReport.ps1
+++ Get-BoatReport.ps1
@@ -1,5 +1,9 @@
 Function Get-BoatReport {
     $GPSData = gpspipe -w -n 10 | ConvertFrom-Json | Where-Object Class -EQ "TPV" | Where-Object lat -ne $null
+    $GPSLatitude   = ($GPSData.lat | Measure-Object -Average).Average
+    $GPSLongtitude = ($GPSData.lon | Measure-Object -Average).Average
+    $GPSSpeed      = ($GPSData.speed  | Measure-Object -Average).Average
+
     $IpifyRequest = Invoke-WebRequest -Uri api.ipify.org
 
     New-Object -TypeName psobject -Property @{
@@ -11,10 +15,10 @@ Function Get-BoatReport {
             InternalIP    = ((hostname -I).Split(' ') | Select-Object -SkipLast 1)
         }
         GPS               = @{
-            Latitude   = ($GPSData.lat | Measure-Object -Average).Average
-            Longtitude = ($GPSData.lon | Measure-Object -Average).Average
-            Speed      = ($GPSData.speed  | Measure-Object -Average).Average
-            GoogleMaps = "https://www.google.com/maps/place/$($GPSData.lat),$($GPSData.lon)"
+            Latitude   = $GPSLatitude
+            Longtitude = $GPSLongtitude
+            Speed      = $GPSSpeed
+            GoogleMaps = "https://www.google.com/maps/place/$GPSLatitude,$GPSLongtitude"
         }
     }
 }

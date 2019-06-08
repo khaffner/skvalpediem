b1781216dfda4e8cdf48d557c4f97cf5d6f440ad
Kevin Haffner <khaffner@gmail.com>
2019-06-08 16:28:50 +0200

    new names

--- Run.ps1
+++ Run.ps1
@@ -1,17 +1,16 @@
 . $PSScriptRoot/Get-BoatReport.ps1
 
-$DataFile = "$env:HOME/boatdata/data.json"
+$DataDir = "$env:HOME/boatdata"
 
-$DataDir = Split-Path $DataFile -Parent
 if(!(Test-Path $DataDir)) {
     New-Item $Datadir -ItemType Directory
 }
 
-vnstati -vs -i enxb827eb2b9087+wlan0 -o /home/pi/boatdata/datausage.png
+vnstati -vs -i enxb827eb2b9087+wlan0 -o "$DataDir/datausage.png"
 
 $NewData = Get-BoatReport
 
 [object[]]$Data = @()
-$Data += (Get-Content $DataFile -ErrorAction SilentlyContinue | ConvertFrom-Json)
+$Data += (Get-Content "$DataDir/rawdata.json" -ErrorAction SilentlyContinue | ConvertFrom-Json)
 $Data += $NewData
 $Data | ConvertTo-Json -Depth 10 | Out-File -FilePath $DataFile -Force | Out-Null

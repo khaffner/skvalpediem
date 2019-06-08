3e7cbdfeec609540858c033611e3737dd9ea1aac
Kevin Haffner <khaffner@gmail.com>
2019-06-09 01:33:23 +0200

    mocking data if gps not present

--- Get-BoatReport.ps1
+++ Get-BoatReport.ps1
@@ -1,9 +1,16 @@
 Function Get-BoatReport {
-    $GPSData = gpspipe -w -n 10 | ConvertFrom-Json | Where-Object Class -EQ "TPV" | Where-Object lat -ne $null
-    $GPSLatitude = ($GPSData.lat | Measure-Object -Average).Average
-    $GPSLongtitude = ($GPSData.lon | Measure-Object -Average).Average
-    $GPSSpeed = ($GPSData.speed | Measure-Object -Average).Average
-
+    $GPSDevice = "/dev/ttyUSB0"
+    if(Test-Path $GPSDevice) {
+        $GPSData = gpspipe -w -n 10 | ConvertFrom-Json | Where-Object Class -EQ "TPV" | Where-Object lat -ne $null
+        $GPSLatitude = ($GPSData.lat | Measure-Object -Average).Average
+        $GPSLongtitude = ($GPSData.lon | Measure-Object -Average).Average
+        $GPSSpeed = ($GPSData.speed | Measure-Object -Average).Average
+    }
+    else { # Mock some data
+        $GPSLatitude = '59.0000'
+        $GPSLongtitude = '10.0000'
+        $GPSSpeed = '0.0'
+    }
     $IpifyRequest = Invoke-WebRequest -Uri api.ipify.org
     $Processes = Get-Process
     $Uptime = Get-Uptime

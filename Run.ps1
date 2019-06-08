98f44242e383f9ad8a841be35cca410cf0083368
Kevin Haffner <khaffner@gmail.com>
2019-06-08 16:31:57 +0200

    forgot a variable...

--- Run.ps1
+++ Run.ps1
@@ -13,4 +13,4 @@ $NewData = Get-BoatReport
 [object[]]$Data = @()
 $Data += (Get-Content "$DataDir/rawdata.json" -ErrorAction SilentlyContinue | ConvertFrom-Json)
 $Data += $NewData
-$Data | ConvertTo-Json -Depth 10 | Out-File -FilePath $DataFile -Force | Out-Null
+$Data | ConvertTo-Json -Depth 10 | Out-File -FilePath "$DataDir/rawdata.json" -Force | Out-Null

8d24f35da1895cfe7c0a184b70c55aab33bb20c5
Kevin Haffner <khaffner@gmail.com>
2019-06-08 21:41:13 +0200

    some silencing

--- Run.ps1
+++ Run.ps1
@@ -15,5 +15,5 @@ $Data += (Get-Content "$DataDir/rawdata.json" -ErrorAction SilentlyContinue | Co
 $Data += $NewData
 $Data | ConvertTo-Json -Depth 10 | Out-File -FilePath "$DataDir/rawdata.json" -Force | Out-Null
 
-Get-Job -Name webserver | Stop-Job
-Start-Job -Name webserver -ScriptBlock {Set-Location /home/pi/boatdata/web/;python3 -m http.server 8080}
+Get-Job -Name webserver -ErrorAction SilentlyContinue | Stop-Job
+Start-Job -Name webserver -ScriptBlock {Set-Location /home/pi/boatdata/web/;python3 -m http.server 8080} | Out-Null

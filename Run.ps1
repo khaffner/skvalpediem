590ebb1701fd329a91e99fc79bac22c31314190d
Kevin Haffner <khaffner@gmail.com>
2019-06-08 21:32:37 +0200

    webserver

--- Run.ps1
+++ Run.ps1
@@ -6,7 +6,7 @@ if(!(Test-Path $DataDir)) {
     New-Item $Datadir -ItemType Directory
 }
 
-vnstati -vs -i enxb827eb2b9087+wlan0 -o "$DataDir/datausage.png"
+vnstati -vs -i enxb827eb2b9087+wlan0 -o "$DataDir/web/datausage.png"
 
 $NewData = Get-BoatReport
 
@@ -14,3 +14,6 @@ $NewData = Get-BoatReport
 $Data += (Get-Content "$DataDir/rawdata.json" -ErrorAction SilentlyContinue | ConvertFrom-Json)
 $Data += $NewData
 $Data | ConvertTo-Json -Depth 10 | Out-File -FilePath "$DataDir/rawdata.json" -Force | Out-Null
+
+Get-Job -Name webserver | Stop-Job
+Start-Job -Name webserver -ScriptBlock {Set-Location /home/pi/boatdata/web/;python3 -m http.server 8080}

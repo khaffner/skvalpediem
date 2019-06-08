424cbf7cb8e5a623d5176ddbcf5aaa5e6c6b99c6
Kevin Haffner <khaffner@gmail.com>
2019-06-08 14:44:17 +0200

    deeper because vnstat

--- Run.ps1
+++ Run.ps1
@@ -12,4 +12,4 @@ $NewData = Get-BoatReport
 [object[]]$Data = @()
 $Data += (Get-Content $DataFile -ErrorAction SilentlyContinue | ConvertFrom-Json)
 $Data += $NewData
-$Data | ConvertTo-Json -Depth 5 | Out-File -FilePath $DataFile -Force | Out-Null
+$Data | ConvertTo-Json -Depth 10 | Out-File -FilePath $DataFile -Force | Out-Null

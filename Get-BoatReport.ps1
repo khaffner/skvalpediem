75549eb5cc3dada1e9a8fc596c2c3f9df090132e
Kevin Haffner <khaffner@gmail.com>
2019-06-08 12:38:09 +0200

    system info and formatting

--- Get-BoatReport.ps1
+++ Get-BoatReport.ps1
@@ -1,24 +1,39 @@
 Function Get-BoatReport {
     $GPSData = gpspipe -w -n 10 | ConvertFrom-Json | Where-Object Class -EQ "TPV" | Where-Object lat -ne $null
-    $GPSLatitude   = ($GPSData.lat | Measure-Object -Average).Average
+    $GPSLatitude = ($GPSData.lat | Measure-Object -Average).Average
     $GPSLongtitude = ($GPSData.lon | Measure-Object -Average).Average
-    $GPSSpeed      = ($GPSData.speed  | Measure-Object -Average).Average
+    $GPSSpeed = ($GPSData.speed | Measure-Object -Average).Average
 
     $IpifyRequest = Invoke-WebRequest -Uri api.ipify.org
+    $Processes = Get-Process
+    $Uptime = Get-Uptime
 
     New-Object -TypeName psobject -Property @{
         TimeStampRaw      = (Get-Date -Format o)
         TimeStampFriendly = (Get-Date).ToString('dd.MM.yyyy HH:mm')
         Network           = @{
-            HasInternet   = (($IpifyRequest).StatusDescription -EQ "OK")
-            ExternalIP    = $IpifyRequest.Content
-            InternalIP    = ((hostname -I).Split(' ') | Select-Object -SkipLast 1)
+            HasInternet = (($IpifyRequest).StatusDescription -EQ "OK")
+            ExternalIP  = $IpifyRequest.Content
+            InternalIP  = ((hostname -I).Split(' ') | Select-Object -SkipLast 1)
         }
         GPS               = @{
             Latitude   = $GPSLatitude
             Longtitude = $GPSLongtitude
             Speed      = $GPSSpeed
             GoogleMaps = "https://www.google.com/maps/place/$GPSLatitude,$GPSLongtitude"
+        }
+        System            = @{
+            Services = @{
+                syncthing  = ($Processes.Name -contains 'syncthing')
+                gpsd       = ($Processes.Name -contains 'gpsd')
+                ssh        = ($Processes.Name -contains 'sshd')
+                vncagent   = ($Processes.Name -contains 'vncagent')
+                bluetoothd = ($Processes.Name -contains 'bluetoothd')
+            }
+            Uptime   = @{
+                Days  = $Uptime.Days
+                Hours = $Uptime.Hours
+            }
         }
     }
 }

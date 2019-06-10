432823013ee0f063342d398eab3a9a433e08a8be
Kevin Haffner <khaffner@gmail.com>
2019-06-10 10:15:36 +0200

    system page

--- New-Dashboard.ps1
+++ New-Dashboard.ps1
@@ -13,17 +13,26 @@ $Page_Map = New-UDPage -Name Map -Icon map -Title "Map" -Content {
 $Page_NetworkUsage = New-UDPage -Name NetworkUsage -Icon wifi -Title "Network usage" -Content {
     New-UDImage -Path "$env:HOME/boatdata/datausage.png" -Width 320 -Height 240
 }
+$Page_System = New-UDPage -Name System -Icon cogs -Title "System" -Content {
+    New-UDCard -Title "System" -Content {
+        New-UDCollection -Content {
+            New-UDCollectionItem -Content "Uptime: $($Data.System.Uptime.Days) days, $($Data.System.Uptime.Hours) hours"
+        }
+    }
+}
 
 $Navigation = New-UDSideNav -Content {
     New-UDSideNavItem -Text "Home" -PageName "Home" -Icon home
     New-UDSideNavItem -Text "Map" -PageName "Map" -Icon map
     New-UDSideNavItem -Text "Network usage" -PageName "NetworkUsage" -Icon wifi
+    New-UDSideNavItem -Text "System" -PageName "System" -Icon cog
 }
 
 $Dashboard = New-UDDashboard -Title "Skvalpe Diem" -Pages @(
     $Page_Home,
     $Page_Map,
     $Page_NetworkUsage
+    $Page_System
 ) `
 -Navigation $Navigation `
 -Footer (New-UDFooter -Copyright "Last updated: $($Data.TimeStampFriendly)")

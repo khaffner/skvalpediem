f5ae6725f80eee22aa7988289f7e474bb6549e69
Kevin Haffner <khaffner@gmail.com>
2019-06-10 09:36:59 +0200

    use copyright footer as timestamp

--- New-Dashboard.ps1
+++ New-Dashboard.ps1
@@ -1,7 +1,7 @@
 . $PSScriptRoot/Get-BoatReport.ps1
 $Data = Get-BoatReport
 
-$Page_Home = New-UDPage -Name Home -Icon home -Title "Home" -Content {New-UDCard -Title "Last updated: $($Data.TimeStampFriendly)"}
+$Page_Home = New-UDPage -Name Home -Icon home -Title "Home" -Content {New-UDCard -Title "Home"}
 $Page_Map = New-UDPage -Name Map -Icon map -Title "Map" -Content {New-UDHtml -Markup "<iframe src=`"$($Data.GPS.GuleSider)`", width=`"800`" height=`"600`"></iframe>"}
 $Page_NetworkUsage = New-UDPage -Name NetworkUsage -Icon wifi -Title "Network usage" -Content {New-UDImage -Path $env:HOME/boatdata/datausage.png -Width 640 -Height 480}
 
@@ -17,7 +17,7 @@ $Dashboard = New-UDDashboard -Title "Skvalpe Diem" -Pages @(
     $Page_NetworkUsage
 ) `
 -Navigation $Navigation `
--Footer (New-UDFooter -Copyright "Kevin Haffner")
+-Footer (New-UDFooter -Copyright "Last updated: $($Data.TimeStampFriendly)")
 
 Get-UDDashboard | Stop-UDDashboard
 Start-UDDashboard -Dashboard $Dashboard -Name SkvalpeDiem -Port 8080

b6cdb6f4a057dc4707da710cb51443d4ca610967
Kevin Haffner <khaffner@gmail.com>
2019-06-20 14:56:59 +0200

    create and start dashboard

---
+++ Start-Dashboard.ps1
@@ -0,0 +1,25 @@
+#$WeatherHeaders = 'Tid', 'Status', 'Temp', 'Nedbør', 'Vindstyrke', 'Vindbeskrivelse', 'Vindretning'
+$LogDir = "$env:HOME/boatdata/"
+
+### Get Data ###
+$GPSData = Get-ChildItem "$LogDir/gpsdata" | Select-Object -Last 1 | Import-Csv -Delimiter ';'
+
+### Dashboard ###
+$Navigation = New-UDSideNav -Fixed -Content {
+    New-UDSideNavItem -Text "Home" -PageName "home" -Icon home
+    New-UDSideNavItem -Text "Map" -PageName "map" -Icon map
+}
+
+$Dashboard = New-UDDashboard -Title "Skvalpe Diem" -Pages @(
+    New-UDPage -Name Home -Icon home -Title "Home" -Content {
+        New-UDCard -Title "Home"
+    }
+    New-UDPage -Name Map -Icon map -Title "Map" -Content {
+        New-UDHtml -Markup "<iframe src=`"$($GPSData.GuleSider)`", width=`"640`" height=`"480`"></iframe>"
+        New-UDCard -Title "Open in seperate tab" -Content {
+            New-UDLink -Text "Gule sider" -Url $GPSData.GuleSider -Icon external-link -OpenInNewWindow
+        }
+    }
+) -Navigation $Navigation
+
+Start-UDDashboard -Dashboard $Dashboard -Name SkvalpeDiem -Port 8080

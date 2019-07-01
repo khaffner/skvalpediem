949bd101fdea1a9d0dcac160d360bc21844d82ef
Kevin Haffner <khaffner@gmail.com>
2019-07-01 18:46:47 +0200

    do everything at the same time

--- ScheduledJob.ps1
+++ ScheduledJob.ps1
@@ -1,22 +1,17 @@
-param(
-    [string]$Schedule
-)
-
 Get-ChildItem "$env:HOME/code/skvalpediem/Functions" | Import-Module -Force
-$Timestamp = Get-Timestamp
-
 $LogDir = "$env:HOME/boatdata"
 
-if($Schedule -eq '15m') {
-    Get-GPSData | ConvertTo-Csv -Delimiter ';' | Out-File -FilePath "$LogDir/gpsdata/$($Timestamp.TimeStampSortable).csv"
-}
-if($Schedule -eq '1h') {
-    Get-ProcessReport | ConvertTo-Csv -Delimiter ';' | Out-File -FilePath "$LogDir/processreport/$($Timestamp.TimeStampSortable).csv"
-    Get-NetData | Select-Object {$_.InternalIP},ExternalIP,HasInternet | ConvertTo-Csv -Delimiter ';' | Out-File -FilePath "$LogDir/network/$($Timestamp.TimeStampSortable).csv"
-    
-    $g = Get-ChildItem /home/pi/boatdata/gpsdata/ | Select-Object -Last 1 | Import-Csv -Delimiter ';'
-    Get-WeatherReport -Latitude $g.Latitude -Longtitude $g.Longtitude | ConvertTo-Csv -Delimiter ';' | Out-File -FilePath "$LogDir/weatherreport/$($Timestamp.TimeStampSortable).csv"
+$Timestamp = Get-Timestamp
 
-}
-if($Schedule -eq '1d') {
-}
+$GPSData = Get-GPSData
+$GPSData | ConvertTo-Csv -Delimiter ';' | Out-File -FilePath "$LogDir/gpsdata/$($Timestamp.TimeStampSortable).csv"
+
+$ProcessReport = Get-ProcessReport 
+$ProcessReport | ConvertTo-Csv -Delimiter ';' | Out-File -FilePath "$LogDir/processreport/$($Timestamp.TimeStampSortable).csv"
+
+$NetData = Get-NetData | Select-Object { $_.InternalIP }, ExternalIP, HasInternet 
+$NetData | ConvertTo-Csv -Delimiter ';' | Out-File -FilePath "$LogDir/netdata/$($Timestamp.TimeStampSortable).csv"
+
+$WeatherData = Get-WeatherReport -Latitude $GPSData.Latitude -Longtitude $GPSData.Longtitude
+$WeatherData | ConvertTo-Csv -Delimiter ';' | Out-File -FilePath "$LogDir/weatherdata/$($Timestamp.TimeStampSortable).csv"
+

11f72aa38f33bb7ed17d851a872134cb93d43acd
Kevin Haffner <khaffner@gmail.com>
2019-07-02 11:22:17 +0200

    specify path

--- ScheduledJob.ps1
+++ ScheduledJob.ps1
@@ -26,5 +26,5 @@ $WeatherData = Get-WeatherReport -Latitude $GPSData.Latitude -Longtitude $GPSDat
 $WeatherData | Export-Csv -Delimiter ';' -Path "$LogDir/weatherdata/$($Timestamp.TimeStampSortable).csv"
 $WeatherData | Select-Object -Property * -ExcludeProperty IconImgUri | ConvertTo-Html -Charset UTF8 -Head $Header | Out-File -FilePath "$LogDir/webpages/weatherreport.html" -Force
 
-$Template = Get-Content -Path "./indextemplate.html"
+$Template = Get-Content -Path "$PSScriptRoot/indextemplate.html"
 $ExecutionContext.InvokeCommand.ExpandString($template) | Out-File -FilePath "$LogDir/webpages/index.html" -Force
